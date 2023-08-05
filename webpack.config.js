f76a440073bfd71d7c1c80e787db56d8db3eb0f4
Ulyana <ushinkevich@mail.ru>
2023-08-05 17:41:36 +0400

    настройка webpack, build, опцимизация css

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,37 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV;
+
 module.exports = {
     entry: "./index.js",
+    mode: isProduction ? "production" : "development",
     output: {
+        path: path.resolve(__dirname, 'dist'),
         filename: "main.js",
+        clean: true,
     },
+    module: {
+        rules: [
+          { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: "asset/resource",
+          },
+        ]
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{from: "static", to:"static"}],
+        }),
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+        }),
+
+    ]
 };
