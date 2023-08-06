60462bd06ec27a48db476a6cc9c946aa21f9ef28
Ulyana <ushinkevich@mail.ru>
2023-08-06 16:29:21 +0400

    --fix

--- dist/main.js
+++ dist/main.js
@@ -1,107 +1,19 @@
-/* eslint-disable camelcase */
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./level-game.js":
-/*!***********************!*\
-  !*** ./level-game.js ***!
-  \***********************/
+/***/ "./js/card-game.js":
+/*!*************************!*\
+  !*** ./js/card-game.js ***!
+  \*************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   renderLevel: () => (/* binding */ renderLevel)
+/* harmony export */   createCardArray: () => (/* binding */ createCardArray),
+/* harmony export */   defaultCardArray: () => (/* binding */ defaultCardArray)
 /* harmony export */ });
-/* harmony import */ var _wholeCardsSuits_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./wholeCardsSuits.js */ "./wholeCardsSuits.js");
-
-
-function renderLevel(card, appEl) {
-    let levelGame = card.value;
-    const cardsFlipSide = [];
-
-    function getCardsFlipSideArr(levelGame) {
-        for (let i = 0; i < levelGame; i++) {
-            cardsFlipSide.push(
-                `<img id="cards-click" class="game_card__flip" src="./static/img/рубашка.png">`,
-            );
-        }
-        return cardsFlipSide;
-    }
-
-    getCardsFlipSideArr(levelGame);
-
-    const appHTML = `
-    <div class="game_card__box">
-            <header class="header">
-                <div class="header_game__timer">  
-                   <div class="game_timer">
-                       <p class="game_timer__item">min</p>
-                       <p class="game_timer__item">sek</p>
-                   </div>   
-                    <p class="game_timer__time">00.00</p>         
-                </div>
-                <button class="header_game__button" id="submit-button" type="submit">Начать заново</button>
-            </header>
-            <div class="game_card">
-            ${cardsFlipSide}
-            </div>
-        </div>`;
-
-    appEl.innerHTML = appHTML;
-
-    let button = document.querySelector('.game_card__flip');
-
-    button.addEventListener('click', (event) => {
-        event.preventDefault();
-
-        const appHTML = `
-         <div class="game_card__box">
-            <header class="header">
-                <div class="header_game__timer">  
-                   <div class="game_timer">
-                       <p class="game_timer__item">min</p>
-                       <p class="game_timer__item">sek</p>
-                   </div>   
-                    <p class="game_timer__time">00.00</p>         
-                </div>
-                <button class="header_game__button" id="submit-button" type="submit">Начать заново</button>
-            </header>
-            <div class="game_card">
-            ${_wholeCardsSuits_js__WEBPACK_IMPORTED_MODULE_0__.cardsSuitsArr}
-            </div>
-        </div>`;
-
-        appEl.innerHTML = appHTML;
-    });
-}
-
-
-/***/ }),
-
-/***/ "./style.css":
-/*!*******************!*\
-  !*** ./style.css ***!
-  \*******************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-// extracted by mini-css-extract-plugin
-
-
-/***/ }),
-
-/***/ "./wholeCardsSuits.js":
-/*!****************************!*\
-  !*** ./wholeCardsSuits.js ***!
-  \****************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   cardsSuitsArr: () => (/* binding */ cardsSuitsArr)
-/* harmony export */ });
-const cardsSuitsArr = [
+const createCardArray = [
     `<img class="game_card__flip" src="./static/img/6 бубны.png" alt="6 бубны">`,
     `<img class="game_card__flip" src="./static/img/6 крести.png" alt="6 крести">`,
     `<img class="game_card__flip" src="./static/img/6 пики.png" alt="6 пики">`,
@@ -147,6 +59,137 @@ const cardsSuitsArr = [
     `<img class="game_card__flip" src="./static/img/туз пики.png" alt="туз пики">`,
     `<img class="game_card__flip" src="./static/img/туз черви.png" alt="туз черви">`,
 ];
+
+//массив рубашек
+const defaultCardArray = [];
+
+for (let i = 0; i < 36; i++) {
+    defaultCardArray.push(
+        `<img class="game_card__flip" data-index="${i}" src="./static/img/рубашка.png">`,
+    );
+}
+
+
+/***/ }),
+
+/***/ "./js/listHtml-game.js":
+/*!*****************************!*\
+  !*** ./js/listHtml-game.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   renderListHtml: () => (/* binding */ renderListHtml)
+/* harmony export */ });
+function renderListHtml(currentCardArr, appEl) {
+    const appHTML = `
+    <div class="game_card__box">
+            <header class="header">
+                <div class="header_game__timer">  
+                   <div class="game_timer">
+                       <p class="game_timer__item">min</p>
+                       <p class="game_timer__item">sek</p>
+                   </div>   
+                    <p class="game_timer__time">00.00</p>         
+                </div>
+                <button class="header_game__button" id="submit-button" type="submit">Начать заново</button>
+            </header>
+            <div class="game_card" id="suits">
+            ${currentCardArr.join('')}
+            </div>
+        </div>`;
+
+    appEl.innerHTML = appHTML;
+}
+
+
+/***/ }),
+
+/***/ "./js/play-game.js":
+/*!*************************!*\
+  !*** ./js/play-game.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   playGame: () => (/* binding */ playGame)
+/* harmony export */ });
+/* harmony import */ var _card_game_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./card-game.js */ "./js/card-game.js");
+/* harmony import */ var _listHtml_game_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./listHtml-game.js */ "./js/listHtml-game.js");
+
+
+
+function playGame(card, appEl) {
+    let lengthArray = card.value;
+    let currentCardArr = [];
+
+    let flag = true;
+
+    let firstCard = null;
+    let secondCard = null;
+
+    let sortCardArray = _card_game_js__WEBPACK_IMPORTED_MODULE_0__.createCardArray
+        .sort(() => Math.random() - 0.5)
+        .slice(0, lengthArray / 2);
+    sortCardArray = sortCardArray
+        .concat(sortCardArray)
+        .sort(() => Math.random() - 0.5);
+
+    let sortDefaultCardArr = _card_game_js__WEBPACK_IMPORTED_MODULE_0__.defaultCardArray.slice(0, lengthArray);
+    currentCardArr = sortDefaultCardArr;
+
+    (0,_listHtml_game_js__WEBPACK_IMPORTED_MODULE_1__.renderListHtml)(sortCardArray, appEl);
+
+    function showCoverCard() {
+        (0,_listHtml_game_js__WEBPACK_IMPORTED_MODULE_1__.renderListHtml)(currentCardArr, appEl);
+
+        const suits = document.getElementById('suits');
+        let itemCards = suits.children;
+
+        for (const itemCard of itemCards) {
+            itemCard.addEventListener('click', () => {
+                let cardIndex = itemCard.dataset.index;
+                //let duplicate = itemCard.dataset.index;
+
+                if (flag) {
+                    firstCard = cardIndex;
+                    currentCardArr[cardIndex] = sortCardArray[cardIndex];
+                    showCoverCard();
+                } else {
+                    secondCard = cardIndex;
+                    compareCard(firstCard, secondCard);
+                    showCoverCard(); //нет картинки
+                }
+                flag = !flag;
+            });
+        }
+    }
+    setTimeout(showCoverCard, 5000);
+
+    function compareCard(firstCard, secondCard) {
+        if (sortCardArray[firstCard] === sortCardArray[secondCard]) {
+            currentCardArr = sortCardArray;
+            alert('Выиграл');
+        } else {
+            currentCardArr = sortCardArray;
+            alert('Проиграл');
+        }
+    }
+}
+
+
+/***/ }),
+
+/***/ "./style.css":
+/*!*******************!*\
+  !*** ./style.css ***!
+  \*******************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
 
 
 /***/ })
@@ -214,14 +257,14 @@ var __webpack_exports__ = {};
   !*** ./index.js ***!
   \******************/
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ "./style.css");// eslint-disable-line no-unused-vars
-/* harmony import */ var _level_game_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./level-game.js */ "./level-game.js");
+/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ "./style.css");
+/* harmony import */ var _js_play_game_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./js/play-game.js */ "./js/play-game.js");
 
 
 
-function renderFormSelectionLevel() {
-    let appEl = document.getElementById('app');
+let appEl = document.getElementById('app');
 
+function renderFormSelectionLevel(appEl, renderListHtml) {
     const appHTML = `
     <div class="center">
     <form class="difficulty_box" id="form-selection" action="#">
@@ -248,36 +291,21 @@ function renderFormSelectionLevel() {
 
     document
         .getElementById('form-selection')
-        .addEventListener('submit', (event) => {
-            event.preventDefault();
+            .addEventListener('submit', (event) => {
+                event.preventDefault();
 
-            let level = document.querySelectorAll('.button_level__radio');
+                let level = document.querySelectorAll('.button_level__radio');
 
-            for (const card of level) {
-                if (card.checked) {
-                    (0,_level_game_js__WEBPACK_IMPORTED_MODULE_1__.renderLevel)(card, appEl);
+                for (const card of level) {
+                    if (card.checked) {
+                        renderListHtml(card, appEl);
+                    }
                 }
-            }
-        });
+            });
 
-    /* другой вариант обработки клика
-    let level = document.querySelectorAll('.button_level__radio');
-    let button = document.querySelector('.button_start');
-
-    button.addEventListener('click', (event) => {
-        event.preventDefault();
-
-        for (let card of level) {
-            if (card.checked) {
-                renderLevel(card, appEl);
-            }
-        }
-    });
-    */
 }
 
-renderFormSelectionLevel();
-
+renderFormSelectionLevel(appEl, _js_play_game_js__WEBPACK_IMPORTED_MODULE_1__.playGame);
 })();
 
 /******/ })()
