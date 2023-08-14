e6d232b1bdf80ef988b43b44360203664c3ef099
Ulyana <ushinkevich@mail.ru>
2023-08-14 18:38:58 +0400

    тестирование массива рубашек карт

---
+++ test/card-game.test.ts
@@ -0,0 +1,17 @@
+import {describe, expect, test} from '@jest/globals';
+import {defaultCardArray} from '../js/card-game';
+
+describe('Проверяем полученный массив',() => {
+    
+    test('массив не пустой',() => {
+        expect(defaultCardArray).not.toBeNull()
+    })
+
+    test('массив включает в себя строку',() => {
+        expect(defaultCardArray).toContain('`<img class="game_card__flip" data-index="${i}" src="./static/img/рубашка.png">`')
+    })
+
+    test('массив длинной 36 карт',() => {
+        expect(defaultCardArray).toHaveLength(36)
+    })
+})
